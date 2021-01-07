1400a067b5c23b3081a4df46a16bf206f54e1656
klea-allushaj <61850991+klea-allushaj@users.noreply.github.com>
2021-01-07 16:46:36 +0000

    Update kanban.js

--- js/kanban.js
+++ js/kanban.js
@@ -22,6 +22,8 @@ item.id='item-' + order;
 item.draggable=true;
 item.addEventListener('dragstart',event=>event.dataTransfer.setData('text',event.target.id));
 item.addEventListener('dragend',event=>event.dataTransfer.clearData());
+let input=document.createElement('input');
+item.appendChild(input);
 
 
 };
