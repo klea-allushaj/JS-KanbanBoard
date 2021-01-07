9f642be320c76f554a4cfa103c27281733ece779
klea-allushaj <61850991+klea-allushaj@users.noreply.github.com>
2021-01-07 15:12:51 +0000

    Update kanban.js

--- js/kanban.js
+++ js/kanban.js
@@ -20,6 +20,9 @@ const create_item = () => {
   item.classList.add('item');
   item.id='item-' + order;
   item.draggable=true;
+  item.addEventListener('dragstart', event => event.dataTransfer.setData('text', event.target.id));
+  item.addEventListener('dragend', event => event.dataTransfer.clearData());
+
 };
 
 document.querySelectorAll('.drop').forEach(element => {
