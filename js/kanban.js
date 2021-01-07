017b4501a7938683636597577c725e4787397345
klea-allushaj <61850991+klea-allushaj@users.noreply.github.com>
2021-01-07 16:59:28 +0000

    Update kanban.js

--- js/kanban.js
+++ js/kanban.js
@@ -28,9 +28,16 @@ let save_btn=document.createElement('button');
 save_btn.innerHTML='Save';
 save_btn.addEventListener('click',()=>{
   error.innerHTML='';
-  if(input.value != 0){
-    
+  if(input.value != ''){
+    order=order+1;
+    item.innerHTML=input.value;
+    adding=false;
   }
+  else{
+    error.innerHTML=message;
+  }
+  item.appendChild(save_btn);
+  return item;
 });
 
 };
